70252e33c849261cf5a1961004717de36fff4be7
Matheus Nunes França <matheus62574156@edu.df.senac.br>
2026-09-23 17:33:21 -0300

    updade card

--- javascript/xuhxuuh.js
+++ javascript/xuhxuuh.js
@@ -17,12 +17,10 @@ if (FORM_CARD){
         const listaCards = JSON.parse(localStorage.getItem("catalogoCArds")) || [] 
         
         listaCards.push(novoCard);
-
         localStorage.setItem("catalogoCards", JSON.stringify(listaCards));
 
         alert("Item adicionado ao catálogo com sucesso!");
-
-        window.location.href = "card.html";
+        window.location.href = "index.html";
 
     });
 
@@ -34,13 +32,13 @@ if (containerCatalogo) {
     const listaCards = JSON.parse(localStorage.getItem("catalogoCards")) || []
 
 
-    containerCatalogo.innerText = " ";
+    containerCatalogo.innerText = "";
 
         if(listaCards.length === 0) {
-            containerCatalogo.innerText = '<p style="text-aling": center; grid-colum: 1/-1; color>Nenhum item cadastrado ainda</p>; <span class="preco">RS $ {card-preco}</span></div>';
+            containerCatalogo.innerText = '<p style="text-aling: center; grid-colum: 1/-1; color #7777;" >Nenhum item cadastrado ainda</p>; <span class="preco">RS $ {card-preco}</span></div>';
         }else {
             listaCards.forEach(function(card){
-            const cardHTML = '<div class="card-item" data-id="${card.id}"><img src="${card.imagemUrl}"alt="${card-titulo}" onerror="this.src="https://picsum.photos/300/200"><div> <h3>${card-titulo}</h3> <p>${card-descricao}</p> </div>';
+            const cardHTML = '<div class="card-item" data-id="${card.id}"><img src="${card.imagemUrl}" alt="${card-titulo}" onerror="this.src="`https://picsum.photos/300/200"><div> <h3>${card-titulo}</h3> <p>""{card-descricao}</p> <span class="preco">RS ${card-preco}</span> </div>';
 
             containerCatalogo.innerHTML += cardHTML;
         });
